5907282f1f66ffc8e6f039e2d882b7254a055ef9
Baldomo <leobaldin.2000@gmail.com>
2018-03-25 11:29:55 +0200

    Aggiunto buildQuery in dbmanager.go, aggiornato agenda_table.sql

---
+++ server/server_testa.go
@@ -0,0 +1,74 @@
+package server
+
+import (
+	//"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestServers(t *testing.T) {
+	t.Parallel()
+	//assert := assert.New(t)
+	tests := []struct {
+		desc         string
+		endpoint     string
+		expectedCode int
+		method       string
+	}{
+		{
+			"Endpoint classi",
+			"/classi",
+			http.StatusOK,
+			"GET",
+		}, {
+			"Endpoint comunicati",
+			"/comunicati",
+			http.StatusOK,
+			"GET",
+		}, {
+			"Endpoints comuncati docenti",
+			"/comunicati/docenti",
+			http.StatusOK,
+			"GET",
+		}, {
+			"Endpoint comunicati genitori",
+			"/comunicati/genitori",
+			http.StatusOK,
+			"GET",
+		}, {
+			"Endpoint comunicati studenti",
+			"/comunicati/studenti",
+			http.StatusOK,
+			"GET",
+		}, {
+			"Endpoint docenti",
+			"/docenti",
+			http.StatusOK,
+			"GET",
+		},
+	}
+
+	go Handler.Start()
+	<-Handler.Started
+
+	for _, test := range tests {
+		req, _ := http.NewRequest(test.method, test.endpoint, nil)
+		resp := executeRequest(req)
+		checkResponseCode(t, test.expectedCode, resp.Code)
+	}
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	Handler.https.Handler.ServeHTTP(rr, req)
+	Handler.http.Handler.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
